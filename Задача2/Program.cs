dbd18ebf21432717bc0a3972a5497f5b238ff65d
Valentin <zvezdochet.79@mail.ru>
2022-03-22 17:05:48 +0300

    Задача2

---
+++ Задача2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+string Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    return strValue;
+}
+
+string number = Prompt("Введите число: ");
+
+int result = int.Parse(number[0].ToString());
+Console.Write($"{number[0]}");
+int len = number.Length;
+for( int i = 1; i < len; i++)
+{
+    result = result + int.Parse(number[i].ToString());
+    Console.Write($" + {number[i]}");
+}
+
+Console.Write($" = {result}");
