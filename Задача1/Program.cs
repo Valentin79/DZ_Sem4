768399948fbdf44a3553ea0fac6c33ebe50bc9f0
Valentin <zvezdochet.79@mail.ru>
2022-03-22 16:06:05 +0300

    Задача1

---
+++ Задача1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int A = Prompt("Введите число A: ");
+int B = Prompt("Введите число B: ");
+
+while(B < 1) 
+{
+    Console.WriteLine("Неверный параметр(В < 1)");
+    B = Prompt("Введите число B: ");
+}
+
+int i = 1;
+int result = 1;
+while(i <= B)
+{
+    result = result * A;
+    i++;
+}
+
+Console.WriteLine(result);
